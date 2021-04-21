00b51f758ec88af2a52fd80b3e36acdf032daf87
Nidhi <54665514+NidhiR88@users.noreply.github.com>
2021-04-21 12:29:04 +0530

    updated login page

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react"
 
 const Login = () => {
+const [ username, setUsername] = useState('');
+const [ password, setPassword] = useState('')
+
+const handleChange = (e) => {
+
+  if(e.target.name === "username"){
+    setUsername(e.target.value);
+  }
+  else {
+    setPassword(e.target.value);
+  }
+}
+
   return (
     <Flex width="full" align="center" justifyContent="center">
       <Box p={2}>
@@ -12,11 +25,11 @@ const Login = () => {
           <form>
             <FormControl>
               <FormLabel>Email</FormLabel>
-              <Input type="email" placeholder="Enter your email" />
+              <Input type="email" name= "username" value={username} placeholder="Enter your email " onChange={handleChange} />
             </FormControl>
             <FormControl mt={6}>
               <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder="*******" />
+              <Input type="password" name="password" value ={password} placeholder="*******" onChange={handleChange} />
             </FormControl>
             <Button width="full" mt={4} type="submit">
               Sign In
