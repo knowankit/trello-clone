39da33b1f391a2350bedf92c0aa81e1cff7204d9
knowankit <44323532+knowankit@users.noreply.github.com>
2021-04-24 22:24:20 +0530

    fix: make home page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,27 @@ const HomePage = () => {
     <>
       <Box bgGradient="linear(darkblue, white)" height="100vh">
         <NavBar />
-        <Flex alignItems="center" justifyContent="center" p="4rem">
-          <Box height="40px">
-            <Text fontSize="5xl">Trello helps teams move work forward.</Text>
-            <Text fontSize="2xl" width="50%">
+        <Flex
+          alignItems="center"
+          flexDirection={['column', 'column', 'row', 'row']}
+          justifyContent="center"
+          p="4rem">
+          <Box>
+            <Text fontSize={['40px', '40px', '50px', '50px']} fontWeight="bold" lineHeight="50px">
+              Trello helps teams move work forward.
+            </Text>
+            <Text
+              fontSize={['1rem', '1rem', '1.5rem', '1.5rem']}
+              width={['100%', '100%', '50%', '50%']}>
               Collaborate, manage projects, and reach new productivity peaks. From high rises to the
-              home office, the way your team works is unique—accomplish it all with Trello.
+              home office, the way your team works is unique - accomplish it all with Trello.
             </Text>
           </Box>
           <Box>
-            <Image height="500px" src="/home-illustration.svg" alt="brand logo"></Image>
+            <Image
+              height={['200px', '300px', '400px', '500px']}
+              src="/home-illustration.svg"
+              alt="brand logo"></Image>
           </Box>
         </Flex>
       </Box>
