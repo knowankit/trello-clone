035feb063bc942e4c7aaee09779c1c98917434de
knowankit <44323532+knowankit@users.noreply.github.com>
2021-05-02 12:33:04 +0530

    fix: prod variable

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,5 @@
 export default function checkEnvironment() {
-  const envUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'http://trello-clone-one.vercel.app';
+  const envUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
 
   return envUrl;
 }
