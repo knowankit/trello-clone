01a5c04b1fc6a5ea4dd1103a9d8afa6b829c744a
knowankit <44323532+knowankit@users.noreply.github.com>
2021-05-17 18:38:11 +0530

    feat: add JWT in login page

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,23 +1,44 @@
-export default function handler(req, res) {
+import { connectToDatabase } from '@/util/mongodb';
+import { compare } from 'bcrypt';
+
+import { sign } from 'jsonwebtoken';
+
+const KEY = process.env.JWT_SECRET_KEY;
+
+const isUserExists = async (db, email) => {
+  const user = await db.collection('users').findOne({ email: email });
+
+  if (user) {
+    return user;
+  }
+
+  return null;
+};
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const body = req.body;
+    const { email, password } = req.body;
 
-    if (body.email === 'nidhi@gmail.com' && body.password === 'learning') {
-      const data = {
-        message: 'success',
-        status: 200
-      };
+    // Check any field is empty
+    if (!email || !password) res.status(400).send({ message: 'email or password is missing' });
 
-      res.send(data);
-    } else {
-      const data = {
-        message: 'Invalid username of password',
-        status: 404
-      };
+    const { db, client } = await connectToDatabase();
 
-      res.send(data);
+    if (client.isConnected()) {
+      const userDetail = await isUserExists(db, email);
+
+      if (userDetail) {
+        compare(password, userDetail.password, function (err, isMatched) {
+          if (isMatched === true) {
+            const claim = { email: userDetail.email };
+            const token = sign(claim, KEY, { expiresIn: '1h' });
+
+            res.send({ token, status: 200 });
+          }
+        });
+      } else {
+        res.status(404).send({ message: 'User does not exists' });
+      }
     }
-  } else {
-    // Handle any other HTTP method
   }
 }
