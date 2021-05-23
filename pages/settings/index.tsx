466c2fe54a7817d0895921d3a7f60615463374b7
knowankit <44323532+knowankit@users.noreply.github.com>
2021-05-23 17:52:50 +0530

    fix: add cookie validation in all the pages

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Settings from '@/src/components/settings';
 import { Box } from '@chakra-ui/layout';
 import withSidebar from '@/src/hoc/with-sidebar';
+import isValidUser from '@/util/is-valid-user';
+import withStore from '@/src/hoc/with-store';
+import { setOrGetStore } from '@/util/initialise-store';
 
-const SettingsPageWithSidebar = withSidebar(Settings, { page: 'settings' });
-
-const HomePage = () => {
+const SettingsPage = () => {
   return (
     <>
       <Box height="100vh">
@@ -15,4 +16,24 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+const SettingsPageWithSidebar = withSidebar(Settings, { page: 'settings' });
+const SettingsPageWithStore = withStore(SettingsPageWithSidebar);
+
+SettingsPage.getInitialProps = async (ctx) => {
+  const reduxStore = setOrGetStore();
+  const isValid = isValidUser(ctx);
+
+  if (!isValid && typeof window === 'undefined') {
+    ctx.res.writeHead(307, {
+      Location: '/login'
+    });
+
+    ctx.res.end();
+  }
+
+  return {
+    initialReduxStore: reduxStore.getState()
+  };
+};
+
+export default SettingsPage;
--- pages/templates/index.tsx
+++ pages/templates/index.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 import Templates from '@/src/components/templates';
 import { Box } from '@chakra-ui/layout';
 import withSidebar from '@/src/hoc/with-sidebar';
-import NavBar from '@/src/components/navbar';
+import isValidUser from '@/util/is-valid-user';
+import withStore from '@/src/hoc/with-store';
+import { setOrGetStore } from '@/util/initialise-store';
 
 const TemplatesPageWithSidebar = withSidebar(Templates, { page: 'templates' });
+const HomePageWithStore = withStore(TemplatesPageWithSidebar);
 
-const HomePage = () => {
-  return (
-    <>
-      <Box height="100vh">
-        <TemplatesPageWithSidebar />
-      </Box>
-    </>
-  );
+HomePageWithStore.getInitialProps = async (ctx) => {
+  const reduxStore = setOrGetStore();
+  const isValid = isValidUser(ctx);
+
+  if (!isValid && typeof window === 'undefined') {
+    ctx.res.writeHead(307, {
+      Location: '/login'
+    });
+
+    ctx.res.end();
+  }
+
+  return {
+    initialReduxStore: reduxStore.getState()
+  };
 };
 
-export default HomePage;
+export default HomePageWithStore;
