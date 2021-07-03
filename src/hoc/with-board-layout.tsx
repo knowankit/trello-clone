727d08beb87cccbe947a9a17b6d9928b1e3cc558
knowankit <44323532+knowankit@users.noreply.github.com>
2021-07-03 19:17:48 +0530

    fix: dnd in server side

--- src/hoc/with-board-layout.tsx
+++ src/hoc/with-board-layout.tsx
@@ -3,6 +3,7 @@ import { setOrGetStore } from '@/util/initialise-store';
 import { fetchColumns } from '@/src/slices/columns';
 import { fetchBoard } from '@/src/slices/board';
 import { fetchCards } from '@/src/slices/cards';
+import { resetServerContext } from 'react-beautiful-dnd';
 
 const WithBoardLayout = (App) => {
   return class AppWithBoardLayout extends Component {
@@ -12,6 +13,10 @@ const WithBoardLayout = (App) => {
 
     static async getInitialProps(ctx) {
       let appProps = {};
+
+      // This is important for react-beautifull-dnd to work
+      // https://github.com/atlassian/react-beautiful-dnd/issues/1756
+      resetServerContext();
 
       if (App.getInitialProps) {
         appProps = await App.getInitialProps(ctx);
